f21556bb3bfeb08b212520980fb3a762f275aef0
Himanshu Mehta <himanshum2497@gmail.com>
2017-07-24 16:51:37 -0700

    Styled the nav bar

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -1,16 +1,49 @@
 const React = require('react');
 const {Link, NavLink} = require('react-router-dom');
 
-class Navigation extends React.Component{
+class Navigation extends React.Component {
+  onSearch(e) {
+    e.preventDefault();
+    alert('Not yet working');
+  }
+
   render() {
     return (
-      <div>
-        <h2>Navigation Component</h2>
-        <NavLink exact to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</NavLink>
-        <NavLink to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</NavLink>
-        <NavLink to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</NavLink>
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Weather Application</li>
+            <li>
+              <NavLink exact to="/" activeClassName="active" activeStyle={{
+                fontWeight: 'bold'
+              }}>Get Weather</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" activeClassName="active" activeStyle={{
+                fontWeight: 'bold'
+              }}>About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/examples" activeClassName="active" activeStyle={{
+                fontWeight: 'bold'
+              }}>Examples</NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li>
+                <input type="search" placeholder="Search weather"/>
+              </li>
+              <li>
+                <input type="submit" className="button" value="Get Weather"/>
+              </li>
+            </ul>
+          </form>
+        </div>
       </div>
-    )
+    );
   }
 }
 
