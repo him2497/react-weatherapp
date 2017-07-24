fb74aba9b61193fa099dc9fc3b3e8d62907a8aa0
Himanshu Mehta <himanshum2497@gmail.com>
2017-07-23 22:35:49 -0700

    changes in server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 const path = require('path');
 
 app.use((req, res, next) => {
-    if(req.headers['x-forwarded-proto'] === 'http'){
-      next();
-    }else{
+    if(req.headers['x-forwarded-proto'] === 'https'){
       res.redirect('http://' + req.hostname + req.url);
+    }else{
+      next();
     }
 });
 
