0ee9d5124f7f6d68fd03f0a272827772e5a96436
Himanshu Mehta <himanshum2497@gmail.com>
2017-07-23 22:10:48 -0700

    Changes to the openWeatherMap.jsx file

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -2,8 +2,6 @@ const axios = require('axios');
 
 const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=bc34ab94d1ba2dab2f612ff516148c4b&units=imperial';
 
-// bc34ab94d1ba2dab2f612ff516148c4b
-// c4e735ea8bd7e7b6dc8368c752517b2d
  function getTemp(location){
   var encodedLocation = encodeURIComponent(location);
   var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
