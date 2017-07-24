16ef22a2d3934a5f8eaffb462a9308e39aff21c0
Himanshu Mehta <himanshum2497@gmail.com>
2017-07-24 16:35:30 -0700

    added Foundation-sites to the application for styling

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,10 @@ const Weather = require('Weather');
 const About = require('About');
 const Examples = require('Examples');
 
+//Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
 ReactDOM.render(
     <BrowserRouter>
       <Main>
